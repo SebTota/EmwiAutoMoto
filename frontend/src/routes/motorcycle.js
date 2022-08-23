bffcf06b466d7bc3c5fc860432f53ff0152a4b5c
SebTota <kidokyo1@gmail.com>
2022-08-22 21:00:58 -0700

    Added detailed motorycle page route

---
+++ frontend/src/routes/motorcycle.js
@@ -0,0 +1,9 @@
+import HeaderNavbar from "../components/navbar";
+
+export default function Motorcycle() {
+  return (
+      <div className="App">
+          <HeaderNavbar />
+      </div>
+  );
+}
