7ad91c1997494eda54df02833995207c7388fad3
SebTota <kidokyo1@gmail.com>
2022-08-25 07:26:10 -0700

    Updated columns of store to fix breaking of rows

--- frontend/src/components/ItemList/index.js
+++ frontend/src/components/ItemList/index.js
@@ -1,48 +1,10 @@
 import React, {Component} from 'react';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
 import ListItem from "../ListItem";
 import { getMotorcycles } from "../../controllers/StoreController";
 
 import "./styles.css"
-
-const numMotorcyclesPerRowExtraSmall = 1;
-const numMotorcyclesPerRowMedium = 2;
-const numMotorcyclesPerRow = 3;
-
-function mapMotorcyclesToRows(motorcycles) {
-    const numRows = Math.ceil(motorcycles.length / numMotorcyclesPerRow);
-
-    let res = [];
-    for (let i = 0, j = 0; i < numRows; i++, j += numMotorcyclesPerRow) {
-        res[i] = [];
-        res[i] = motorcycles.slice(j, j + numMotorcyclesPerRow);
-    }
-
-    return res;
-}
-
-
-class ItemRow extends Component {
-    render() {
-        const addNumEmptyCols = numMotorcyclesPerRow - this.props.item.length;
-        const emptyCols = [];
-
-        for (let i = 0; i < addNumEmptyCols; i++) {
-            emptyCols.push(i);
-        }
-
-        return (
-            <Row xs={numMotorcyclesPerRowExtraSmall} md={numMotorcyclesPerRowMedium} lg={numMotorcyclesPerRow} className="g-4">
-                {this.props.item.map((motorcycle =>
-                    <Col><ListItem key={motorcycle.key} item={motorcycle}/></Col>))}
-                {emptyCols.map((emptyCol => <Col key={emptyCol}></Col>))}
-            </Row>
-        )
-    }
-}
 
 
 class ItemList extends Component {
@@ -67,14 +29,13 @@ class ItemList extends Component {
         if (this.state.motorcycles === null) {
             return (<h3>Loading</h3>)
         } else {
-            const rowMapping = mapMotorcyclesToRows(this.state.motorcycles);
-            let rowNum = 0;
-
             return (
                 <Container className="items">
-                    {
-                        rowMapping.map((row) => <ItemRow key={rowNum++} item={row}></ItemRow>)
-                    }
+                    <div className="row">
+                        {
+                            this.state.motorcycles.map((motorcycle) => <div key={motorcycle.key} className="col-12 col-sm-6 col-md-4"><ListItem key={motorcycle.key} item={motorcycle}/></div>)
+                        }
+                    </div>
                 </Container>
             );
         }
--- frontend/src/components/ListItem/index.js
+++ frontend/src/components/ListItem/index.js
@@ -9,7 +9,7 @@ class ListItem extends Component {
     render() {
         return(
             <Link to={`/motorcycle/${this.props.item.id}`} style={{ color: 'inherit', textDecoration: 'inherit'}}>
-                <Card>
+                <Card className="motorcycle-card-wrapper">
                     <Card.Img variant="top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/2013_Kawasaki_Ninja_300_Seattle_Motorcycle_Show.jpg/1920px-2013_Kawasaki_Ninja_300_Seattle_Motorcycle_Show.jpg" />
                     <Card.Body className="shop-item-card">
                         <Row><Col className="shop-item-card-title-col"><span>{this.props.item.year + ' ' + this.props.item.make + ' ' + this.props.item.model}</span></Col></Row>
