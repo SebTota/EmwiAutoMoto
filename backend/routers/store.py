99ceb22761aa08d5662ade1d0f80a906844506b8
SebTota <kidokyo1@gmail.com>
2022-08-25 07:10:49 -0700

    sort motorcycle results by newest added

--- backend/routers/store.py
+++ backend/routers/store.py
@@ -16,8 +16,10 @@ def get_motorcycles(limit: int = 9, show_sold: bool = False, pagination_cursor: 
 
     if pagination_cursor:
         motorcycles_controller = motorcycles_controller.cursor(pagination_cursor)
-    elif not show_sold:
-        motorcycles_controller = motorcycles_controller.filter(sold=False)
+    else:
+        motorcycles_controller = motorcycles_controller.order('-date_created')
+        if not show_sold:
+            motorcycles_controller = motorcycles_controller.filter(sold=False)
 
     motorcycles_controller = motorcycles_controller.fetch(limit)
 
