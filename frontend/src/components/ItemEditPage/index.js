394b99fa2f5b476d032a77725bea180f8ee14e17
SebTota <kidokyo1@gmail.com>
2022-08-27 08:03:21 -0700

    Populate edit form with existing values when updating a motorcycle

--- frontend/src/components/ItemEditPage/index.js
+++ frontend/src/components/ItemEditPage/index.js
@@ -10,79 +10,84 @@ import {getMotorcycle} from "../../controllers/StoreController";
 
 import "./styles.css"
 import "react-image-gallery/styles/css/image-gallery.css";
-import {Dropdown} from "react-bootstrap";
 
 export default function ItemPage(props) {
-    const navigate = useNavigate();
     const [motorcycle, setMotorcycle] = React.useState(null);
-    const [apiCall, setApiCall] = React.useState(false);
-    const [isAdmin, setIsAdmin] = React.useState(true);
-    const [isEditing, setIsEditing] = React.useState(false);
+    /**
+     * Loading - Page Loading
+     * Update - Updating an existing motorcycle
+     * New - Adding a new motorcycle
+     */
+    const changeLoading = "Loading";
+    const changeUpdate = "Update";
+    const changeNew = "New";
+    const [typeOfChange, setTypeOfChange] = React.useState(changeLoading); // TODO: Make this an Enum
 
     // componentDidMount()
     React.useEffect(() => {
-        console.log(apiCall)
-        if (apiCall === false) {
-            setApiCall(true)
+        if (props.id !== 'new') {
             getMotorcycle(props.id).then(motorcycle => {
-                setMotorcycle(motorcycle)
+                setMotorcycle(motorcycle);
+                setTypeOfChange(changeUpdate);
             })
+        } else {
+            setTypeOfChange(changeNew);
         }
-
     }, []);
 
-    function getTitle() {
-        return `${motorcycle.year} ${motorcycle.make} ${motorcycle.model}`;
+    function getYear() {
+        if (typeOfChange === changeUpdate) {
+            return motorcycle.year;
+        }
+    }
+
+    function getMake() {
+        if (typeOfChange === changeUpdate) {
+            return motorcycle.make;
+        }
+    }
+
+    function getModel() {
+        if (typeOfChange === changeUpdate) {
+            return motorcycle.model;
+        }
+    }
+
+    function getColor() {
+        if (typeOfChange === changeUpdate) {
+            return motorcycle.color;
+        }
     }
 
     function getPrice() {
-        return `${motorcycle.price} pln`;
-    }
-
-    function getOdometerReading() {
-        return `${motorcycle.km} km`
-    }
-
-    function getListingDescription() {
-        return motorcycle.description
-    }
-
-    function getImages() {
-        let images = motorcycle.images;
-        images.map(image => {
-            if (image.hasOwnProperty('image')) {
-                image['original'] = image['image'];
-                delete image['image'];
-                return image;
-            }
-        })
-        console.log(images)
-        return images;
-    }
-
-    function enableEditing() {
-        console.log('enabling editing')
-        setIsEditing(true);
-    }
-
-    function saveChanges() {
-        setIsEditing(false);
-    }
-
-    let adminButton;
-    if (isAdmin) {
-        if (!isEditing) {
-            adminButton = <Button variant="warning" onClick={() => {
-                enableEditing()
-            }}>Edit</Button>;
-        } else {
-            adminButton = <Button variant="warning" onClick={() => {
-                saveChanges()
-            }}>Save</Button>;
+        if (typeOfChange === changeUpdate) {
+            return motorcycle.price;
         }
     }
 
-    if (motorcycle === null) {
+    function getOdometer() {
+        if (typeOfChange === changeUpdate) {
+            return motorcycle.odometer;
+        }
+    }
+
+    function getOdometerMeasurement() {
+        if (typeOfChange === changeUpdate) {
+            return motorcycle.odometerMeasurement;
+        }
+    }
+
+    function getStatus() {
+        if (typeOfChange === changeUpdate) {
+            return motorcycle.forSale;
+        }
+    }
+
+    function saveChanges() {
+        console.log('changes saved')
+    }
+
+    if (motorcycle === null && (typeOfChange === changeUpdate || typeOfChange === changeLoading)) {
         return (<h3>Loading...</h3>)
     } else {
         return (
@@ -91,35 +96,35 @@ export default function ItemPage(props) {
                     <Row className="mb-3">
                         <Form.Group as={Col} md={2} className="mb-3">
                             <Form.Label>Year</Form.Label>
-                            <Form.Control type="number" placeholder="Year"/>
+                            <Form.Control type="number" placeholder="Year" defaultValue={getYear()}/>
                         </Form.Group>
                         <Form.Group as={Col} md={3} className="mb-3">
                             <Form.Label>Make</Form.Label>
-                            <Form.Control placeholder="Make"/>
+                            <Form.Control placeholder="Make" defaultValue={getMake()}/>
                         </Form.Group>
                         <Form.Group as={Col} md={5} className="mb-3">
                             <Form.Label>Model</Form.Label>
-                            <Form.Control placeholder="Model"/>
+                            <Form.Control placeholder="Model" defaultValue={getModel()}/>
                         </Form.Group>
                         <Form.Group as={Col} md={2} className="mb-3">
                             <Form.Label>Color</Form.Label>
-                            <Form.Control placeholder="Color"/>
+                            <Form.Control placeholder="Color" defaultValue={getColor()}/>
                         </Form.Group>
                     </Row>
                     <Row>
                         <Form.Group as={Col} md={3} className="mb-3">
                             <Form.Label>Price - Zloty/PLN</Form.Label>
-                            <Form.Control type="number" placeholder="Price"/>
+                            <Form.Control type="number" placeholder="Price" defaultValue={getPrice()}/>
                         </Form.Group>
                         <Form.Group as={Col} md={7}>
                             <Row>
                                 <Form.Group as={Col} md={6} className="mb-3">
                                     <Form.Label>Odometer</Form.Label>
-                                    <Form.Control type="number" placeholder="Odometer"/>
+                                    <Form.Control type="number" placeholder="Odometer" defaultValue={getOdometer()}/>
                                 </Form.Group>
                                 <Form.Group as={Col} md={6} className="mb-3">
                                     <Form.Label>Odometer Measurement</Form.Label>
-                                    <Form.Select id="odometerMeasurementSelect">
+                                    <Form.Select id="odometerMeasurementSelect" defaultValue={getOdometerMeasurement()}>
                                         <option>Miles</option>
                                         <option>Kilometers</option>
                                     </Form.Select>
@@ -128,12 +133,13 @@ export default function ItemPage(props) {
                         </Form.Group>
                         <Form.Group as={Col} md={2} className="mb-3">
                             <Form.Label>Status</Form.Label>
-                            <Form.Select id="odometerMeasurementSelect">
+                            <Form.Select id="odometerMeasurementSelect" defaultValue={getStatus()}>
                                 <option>For Sale</option>
                                 <option>Sold</option>
                             </Form.Select>
                         </Form.Group>
                     </Row>
+                    <Button variant="primary" onClick={() => {saveChanges()}}>Save</Button>
                 </Form>
             </div>
         )
