a32064d56d3ab8403b2e7b96c27190efcbcf6c3e
Seb <kidokyo1@gmail.com>
2022-11-27 20:48:30 -0800

    Use reusable api secret key and add access and refresh token longer expiration dates

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,13 +1,13 @@
 import os
-import secrets
 
 from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
     API_V1_STR: str = '/api/v1'
-    SECRET_KEY: str = secrets.token_urlsafe(32)
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = (60 * 24 * 8)
+    SECRET_KEY: str = os.getenv('API_CREDENTIALS_GENERATOR_SECRET_KEY')
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = (60 * 24 * 14)
+    REFRESH_TOKEN_EXPIRE_MINUTES: int = (60 * 24 * 90)
     DATABASE_PASSWORD: str = os.getenv('DATABASE_PASSWORD')
     DATABASE_URL: str = f'postgresql://postgres:{DATABASE_PASSWORD}@198.46.142.2/emwi_auto_moto'
 
