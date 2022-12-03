caaa63ca0983de15d3db4a7025ad30f9964e2493
Seb Tota <kidokyo1@gmail.com>
2022-12-03 10:39:21 -0800

    Added auth handler to login page

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -10,24 +10,45 @@
         <div class="-space-y-px rounded-md shadow-sm">
           <div>
             <label for="username" class="sr-only">Username</label>
-            <input id="username" name="username" type="text" required="" class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Username" />
+            <input v-model="username" id="username" name="username" type="text"  required="" class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Username" />
           </div>
           <div>
             <label for="password" class="sr-only">Password</label>
-            <input id="password" name="password" type="password" autocomplete="current-password" required="" class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Password" />
+            <input v-model="password" id="password" name="password" type="password" autocomplete="current-password" required="" class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Password" />
           </div>
         </div>
         <div>
-          <button type="submit" class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+          <button @click="handleLogin" type="submit" class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
             Sign in
           </button>
         </div>
+        <div v-if="showLoginError" class="text-sm mb-4 w-max text-red-400">Invalid username or password.</div>
       </form>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import {useMainStore} from "@/stores/state";
+import {ref} from 'vue';
+
+const mainStore = useMainStore();
+
+const showLoginError = ref(false);
+const username = ref('');
+const password = ref('');
+
+async function handleLogin(event: Event) {
+  event.preventDefault();
+  showLoginError.value = false;
+  try {
+    await mainStore.actionLogIn(username.value, password.value);
+  } catch (error) {
+    showLoginError.value = true;
+  }
+}
+
+
 </script>
 
 <style>
