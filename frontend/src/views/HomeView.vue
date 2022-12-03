5622a8679be752cb98fc392300a64a9df48ec67e
Seb Tota <kidokyo1@gmail.com>
2022-12-03 10:40:01 -0800

    Created base motorcycle list view

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <MotorcycleList />
   </main>
 </template>
+
+<script setup lang="ts">
+import MotorcycleList from "../components/MotorcycleList.vue";
+</script>
