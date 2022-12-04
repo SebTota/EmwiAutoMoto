b2a39013a9ddbda55e63caa3e2b4d68a789d3ced
Seb Tota <kidokyo1@gmail.com>
2022-12-04 12:43:20 -0800

    Parse date strings to date objects using common axios config

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -3,6 +3,14 @@ import {apiUrl} from '@/env'
 import type {IUser} from "@/interfaces/user";
 import type {IToken} from "@/interfaces/token";
 import type {IMotorcycleList} from "@/interfaces/motorcycle";
+import {handleDates} from "@/utils/dates";
+
+const client = axios.create({ baseURL: apiUrl });
+
+client.interceptors.response.use(originalResponse => {
+  handleDates(originalResponse.data);
+  return originalResponse;
+});
 
 function authHeaders(token: string) {
   return {
@@ -18,21 +26,21 @@ export const api = {
     params.append('username', username);
     params.append('password', password);
 
-    return axios.post<IToken>(`${apiUrl}/api/v1/login/access-token`, params);
+    return client.post<IToken>(`${apiUrl}/api/v1/login/access-token`, params);
   },
   async refreshToken(refreshToken: string) {
     const body = {
       'refresh_token': refreshToken
     }
-    return axios.post<IToken>(`${apiUrl}/api/v1/users/refresh-token`);
+    return client.post<IToken>(`${apiUrl}/api/v1/users/refresh-token`);
   },
   async getMe(token: string) {
-    return axios.get<IUser>(`${apiUrl}/api/v1/users/me`, authHeaders(token));
+    return client.get<IUser>(`${apiUrl}/api/v1/users/me`, authHeaders(token));
   },
   async getMotorcycles(page: number) {
     const params = new URLSearchParams();
     params.append('page', page.toString());
 
-    return axios.get<IMotorcycleList>(`${apiUrl}/api/v1/motorcycles`, { params });
+    return client.get<IMotorcycleList>(`${apiUrl}/api/v1/motorcycles`, { params });
   }
 };
