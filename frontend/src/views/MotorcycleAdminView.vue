3ec61d62c1f3befc6a1d54dae9853eedff1b29bb
Seb Tota <kidokyo1@gmail.com>
2022-12-18 08:32:49 -0800

    nit: updated odometer measurements to Polish

--- frontend/src/views/MotorcycleAdminView.vue
+++ frontend/src/views/MotorcycleAdminView.vue
@@ -38,8 +38,8 @@
               <div class="col-span-6 sm:col-span-3 md:col-span-2">
                 <label for="odometer_measurement" class="block text-sm font-medium text-gray-700">Pomiar Drogomierz</label>
                 <select v-model="odometerMeasurement" id="odometer_measurement" name="odometer_measurement" required class="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
-                  <option value="mi">Miles</option>
-                  <option value="km">Kilometers</option>
+                  <option value="mi">Mil</option>
+                  <option value="km">Kilometrów</option>
                 </select>
               </div>
 
