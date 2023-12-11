0491e384a20710f76df970728c9b736f4c8751fd
Seb <kidokyo1@gmail.com>
2023-12-11 05:00:45 -0800

    Moving to Java Spring from Python FastAPI

---
+++ backend/src/main/java/com/sebtota/automoto/AutomotoApplication.java
@@ -0,0 +1,13 @@
+package com.sebtota.automoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomotoApplication.class, args);
+	}
+
+}
