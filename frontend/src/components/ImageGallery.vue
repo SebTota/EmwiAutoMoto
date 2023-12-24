9fb88ad20ed6b0bd3dfdb20fd84656b2e8eca3ae
Seb <kidokyo1@gmail.com>
2023-12-23 20:24:28 -0500

    Reenable image gallery in edit view.

--- frontend/src/components/ImageGallery.vue
+++ frontend/src/components/ImageGallery.vue
@@ -1,15 +1,15 @@
 <template xmlns="http://www.w3.org/1999/html">
   <div>
-<!--    <VueDraggableNext class="grid grid-cols-6 gap-4" :list="props.images" v-bind="dragOptions">-->
-<!--      <div v-for="image in props.images" :key="image.id" class="hover:bg-slate-10">-->
-<!--        <div class="aspect-w-5 aspect-h-3 w-full overflow-hidden">-->
-<!--          <img :src="image.image_url" class="rounded hover:transparency-80 h-full w-full object-cover object-center" />-->
-<!--        </div>-->
-<!--        <div class="rounded mt-1 text-center">-->
-<!--          <button @click.prevent="deleteImage(image.id)" class="inline-flex justify-center rounded-md bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-rose-600 focus:outline-none focus:ring-2 focus:ring-rose-500 focus:ring-offset-2">Delete</button>-->
-<!--        </div>-->
-<!--      </div>-->
-<!--    </VueDraggableNext>-->
+    <VueDraggableNext class="grid grid-cols-6 gap-4" :list="props.images" v-bind="dragOptions">
+      <div v-for="image in props.images" :key="image.id" class="hover:bg-slate-10">
+        <div class="aspect-w-5 aspect-h-3 w-full overflow-hidden">
+          <img :src="image.image_url" class="rounded hover:transparency-80 h-full w-full object-cover object-center" />
+        </div>
+        <div class="rounded mt-1 text-center">
+          <button @click.prevent="deleteImage(image.id)" class="inline-flex justify-center rounded-md bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-rose-600 focus:outline-none focus:ring-2 focus:ring-rose-500 focus:ring-offset-2">Delete</button>
+        </div>
+      </div>
+    </VueDraggableNext>
   </div>
 </template>
 
