af0fe5283d984d35226f369e9003c75734e24401
Seb <kidokyo1@gmail.com>
2023-12-23 21:38:23 -0500

    Code cleanup and formatting.

--- frontend/src/components/MotorcycleListPagination.vue
+++ frontend/src/components/MotorcycleListPagination.vue
@@ -1,26 +1,26 @@
 <template>
   <div class="pt-6">
-    <nav class="bg-white px-4 pt-3 flex items-center justify-between border-t border-gray-200 sm:px-6 dark:text-gray-400 dark:bg-gray-900 dark:border-gray-400" aria-label="Pagination">
-<!--      <div class="hidden sm:block">-->
-<!--        <p class="text-sm text-gray-700">-->
-<!--          Showing-->
-<!--          {{ ' ' }}-->
-<!--          <span class="font-medium">1</span>-->
-<!--          {{ ' ' }}-->
-<!--          to-->
-<!--          {{ ' ' }}-->
-<!--          <span class="font-medium">10</span>-->
-<!--          {{ ' ' }}-->
-<!--          of-->
-<!--          {{ ' ' }}-->
-<!--          <span class="font-medium">20</span>-->
-<!--          {{ ' ' }}-->
-<!--          results-->
-<!--        </p>-->
-<!--      </div>-->
+    <nav
+      class="bg-white px-4 pt-3 flex items-center justify-between border-t border-gray-200 sm:px-6 dark:text-gray-400 dark:bg-gray-900 dark:border-gray-400"
+      aria-label="Pagination"
+    >
       <div class="flex-1 flex justify-between sm:justify-end">
-        <button :disabled="!props.hasPrevPage" @click="goBack" type="button" class="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white disabled:opacity-25"> Previous </button>
-        <button :disabled="!props.hasNextPage" @click="nextPage" type="button" class="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white disabled:opacity-25"> Next </button>
+        <button
+          :disabled="!props.hasPrevPage"
+          @click="goBack"
+          type="button"
+          class="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white disabled:opacity-25"
+        >
+          Previous
+        </button>
+        <button
+          :disabled="!props.hasNextPage"
+          @click="nextPage"
+          type="button"
+          class="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white disabled:opacity-25"
+        >
+          Next
+        </button>
       </div>
     </nav>
   </div>
@@ -31,8 +31,8 @@ const props = defineProps({
   hasNextPage: Boolean,
   hasPrevPage: Boolean,
   nextPage: Function,
-  prevPage: Function
-})
+  prevPage: Function,
+});
 
 function goBack() {
   if (props.prevPage) {
