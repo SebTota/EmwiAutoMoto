17a368be0b94b8cc783312ad6415866511bbd7a6
Seb <kidokyo1@gmail.com>
2024-02-17 10:29:22 -0800

    Add support for aerich migrations.

--- backend/db/init_db.py
+++ backend/db/init_db.py
@@ -3,13 +3,24 @@ from tortoise import Tortoise
 
 from backend.core.config import settings
 
+# For aerich migrations
+TORTOISE_ORM = {
+    "connections": {
+        "default": settings.DATABASE_URL,
+    },
+    "apps": {
+        "models": {"models": ["backend.models", "aerich.models"], "default_connection": "default"},
+    },
+}
+
 
 def init_db(app) -> None:
     register_tortoise(
         app,
         db_url=settings.DATABASE_URL,
-        modules={'models': ["backend.models"]},
+        modules={'models': ["backend.models", "aerich.models"]},
     )
+
 
 async def init_db_for_script():
     await Tortoise.init(
