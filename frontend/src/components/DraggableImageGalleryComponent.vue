88f57fad5c4aa3482a5e8d8ec495058faef2b180
Seb <kidokyo1@gmail.com>
2024-02-25 18:35:28 -0800

    Translate 'Delete' in admin panel.

--- frontend/src/components/DraggableImageGalleryComponent.vue
+++ frontend/src/components/DraggableImageGalleryComponent.vue
@@ -17,7 +17,7 @@
             @click.prevent="deleteImage(image.image_url)"
             class="inline-flex justify-center rounded-md bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-rose-600 focus:outline-none focus:ring-2 focus:ring-rose-500 focus:ring-offset-2"
           >
-            Delete
+            Usuń
           </button>
         </div>
       </div>
