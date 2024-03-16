b6aebba2b69ed52f21dbb8ea8f42293a49c05c9a
Seb <kidokyo1@gmail.com>
2024-03-16 09:04:04 -0700

    Remove need for headless UI.

--- frontend/src/components/HeaderComponent.vue
+++ frontend/src/components/HeaderComponent.vue
@@ -1,32 +1,32 @@
 <template>
-  <Popover class="relative bg-white dark:text-grey-900 dark:bg-gray-900">
+  <div class="relative bg-white dark:text-grey-900 dark:bg-gray-900">
     <div class="mx-auto sm:px-5">
       <div class="flex items-center justify-between border-gray-100 py-3 pb-6 lg:justify-start lg:space-x-10">
         <div class="flex justify-start flex-1">
-          <router-link :to="{ name: 'home' }">
-            <span class="sr-only">EMWI Auto Moto</span>
+          <router-link :to="{ name: 'home' }"
+            ><span class="sr-only">EMWI Auto Moto</span>
             <img class="h-7 w-auto block sm:hidden" src="/logo-no-border.png" alt="Company Logo" />
-            <img class="h-14 w-auto hidden sm:block" src="/logo-stacked.png" alt="Company Logo" />
-          </router-link>
+            <img class="h-14 w-auto hidden sm:block" src="/logo-stacked.png" alt="Company Logo"
+          /></router-link>
         </div>
         <div class="-my-2 -mr-2 lg:hidden">
-          <PopoverButton
+          <button
+            @click="toggleMobileMenu"
             class="inline-flex items-center justify-center rounded-md bg-white p-2 focus:outline-none dark:bg-gray-900 dark:text-white dark:hover:bg-gray-800"
           >
             <span class="sr-only">Open menu</span>
             <Bars3Icon class="h-6 w-6" aria-hidden="true" />
-          </PopoverButton>
+          </button>
         </div>
-        <PopoverGroup as="nav" class="hidden space-x-4 lg:space-x-10 lg:flex">
+        <nav class="hidden space-x-4 lg:space-x-10 lg:flex">
           <router-link
             v-for="item in tabs"
             :key="item.name"
             :to="{ name: item.page }"
             class="-m-3 flex items-center rounded-md p-3 hover:bg-gray-50 dark:hover:bg-gray-800"
+            ><a class="text-base font-medium">{{ item.name }}</a></router-link
           >
-            <a class="text-base font-medium">{{ item.name }}</a>
-          </router-link>
-        </PopoverGroup>
+        </nav>
         <div class="hidden items-center justify-end lg:flex lg:flex-1 w-0">
           <div v-if="mainStateLoaded && !isLoggedIn">
             <router-link :to="{ name: 'login' }" class="whitespace-nowrap text-base font-medium">
@@ -43,91 +43,81 @@
       </div>
     </div>
 
-    <transition
-      enter-active-class="duration-200 ease-out"
-      enter-from-class="opacity-0 scale-95"
-      enter-to-class="opacity-100 scale-100"
-      leave-active-class="duration-100 ease-in"
-      leave-from-class="opacity-100 scale-100"
-      leave-to-class="opacity-0 scale-95"
+    <div
+      v-if="mobileMenuOpen"
+      class="z-10 absolute inset-x-0 top-0 origin-top-right transform p-2 transition lg:hidden"
     >
-      <PopoverPanel
-        v-slot="{ close }"
-        focus
-        class="z-10 absolute inset-x-0 top-0 origin-top-right transform p-2 transition lg:hidden"
+      <div
+        class="divide-y-2 divide-gray-50 rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white dark:text-gray-400 dark:bg-gray-800"
       >
-        <div
-          class="divide-y-2 divide-gray-50 rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white dark:text-gray-400 dark:bg-gray-800"
-        >
-          <div class="px-5 py-5">
-            <div class="flex items-center justify-between">
-              <div>
-                <img class="h-8 w-auto" src="/logo-no-border.png" alt="EMWI Auto Moto" />
-              </div>
-              <div class="-mr-2">
-                <PopoverButton
-                  class="inline-flex items-center justify-center rounded-md p-2 hover:bg-gray-100 focus:outline-none dark:bg-gray-800 dark:text-white dark:hover:bg-gray-700"
-                >
-                  <span class="sr-only">Close menu</span>
-                  <XMarkIcon class="h-6 w-6" aria-hidden="true" />
-                </PopoverButton>
-              </div>
+        <div class="px-5 py-5">
+          <div class="flex items-center justify-between">
+            <div>
+              <img class="h-8 w-auto" src="/logo-no-border.png" alt="EMWI Auto Moto" />
             </div>
-            <div class="mt-6">
-              <nav class="grid gap-y-8">
-                <router-link
-                  v-for="item in tabs"
-                  :key="item.name"
-                  :to="{ name: item.page }"
-                  @click="close"
-                  class="-m-3 flex items-center rounded-md p-3 hover:bg-gray-100 dark:hover:bg-gray-700"
-                >
-                  <component :is="item.icon" class="h-6 w-6 flex-shrink-0" aria-hidden="true" />
-                  <span class="ml-3 text-base font-medium">{{ item.name }}</span>
-                </router-link>
-              </nav>
+            <div class="-mr-2">
+              <button
+                @click="toggleMobileMenu"
+                class="inline-flex items-center justify-center rounded-md p-2 hover:bg-gray-100 focus:outline-none dark:bg-gray-800 dark:text-white dark:hover:bg-gray-700"
+              >
+                <span class="sr-only">Close menu</span>
+                <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+              </button>
             </div>
           </div>
-          <div class="space-y-5 py-5 px-5">
-            <div>
-              <link />
-              <div v-if="mainStateLoaded && !isLoggedIn">
-                <router-link
-                  @click="close"
-                  :to="{ name: 'login' }"
-                  class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                >
-                  Zaloguj się
-                </router-link>
-              </div>
-              <div v-else>
-                <button
-                  @click="handleLogout"
-                  type="submit"
-                  class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                >
-                  Wyloguj się
-                </button>
-              </div>
+          <div class="mt-6">
+            <nav class="grid gap-y-8">
+              <router-link
+                v-for="item in tabs"
+                :key="item.name"
+                :to="{ name: item.page }"
+                @click="toggleMobileMenu"
+                class="-m-3 flex items-center rounded-md p-3 hover:bg-gray-100 dark:hover:bg-gray-700"
+              >
+                <component :is="item.icon" class="h-6 w-6 flex-shrink-0" aria-hidden="true" />
+                <span class="ml-3 text-base font-medium">{{ item.name }}</span>
+              </router-link>
+            </nav>
+          </div>
+        </div>
+        <div class="space-y-5 py-5 px-5">
+          <div>
+            <link />
+            <div v-if="mainStateLoaded && !isLoggedIn">
+              <router-link
+                @click="toggleMobileMenu"
+                :to="{ name: 'login' }"
+                class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                Zaloguj się
+              </router-link>
+            </div>
+            <div v-else>
+              <button
+                @click="handleLogout"
+                type="submit"
+                class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                Wyloguj się
+              </button>
             </div>
           </div>
         </div>
-      </PopoverPanel>
-    </transition>
-  </Popover>
+      </div>
+    </div>
+  </div>
 </template>
-
 <script setup lang="ts">
-import { Popover, PopoverButton, PopoverGroup, PopoverPanel } from "@headlessui/vue";
 import { Bars3Icon, XMarkIcon, ArrowLongRightIcon } from "@heroicons/vue/24/outline";
 import { useMainStore } from "@/stores/state";
 import { storeToRefs } from "pinia";
 import { ref } from "vue";
-import { RouteNameEnum } from '@/enums/routeNameEnum'
+import { RouteNameEnum } from "@/enums/routeNameEnum";
 
 const mainState = useMainStore();
 const mainStateLoaded = ref(false);
 const { isLoggedIn, user } = storeToRefs(mainState);
+const mobileMenuOpen = ref(false);
 
 let tabs = [
   {
@@ -154,7 +144,7 @@ const addMotorcycleTab = {
   name: "Nowy Produkt",
   description: "Nowy Produkt",
   icon: ArrowLongRightIcon,
-  page: "newProduct",
+  page: RouteNameEnum.NEW_MOTORCYCLE,
 };
 
 mainState.actionCheckLoggedIn().then(() => {
@@ -172,5 +162,9 @@ async function handleLogout(event: Event) {
   event.preventDefault();
   mainState.actionLogout();
   window.location.reload();
+}
+
+function toggleMobileMenu() {
+  mobileMenuOpen.value = !mobileMenuOpen.value;
 }
 </script>
