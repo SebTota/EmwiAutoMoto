e0449d90169e056d2f2561757ee472304703f3b5
Seb <kidokyo1@gmail.com>
2024-03-19 21:37:02 -0700

    Use db connection pooling and pre-ping to make sure new requests after long inactivity respond faster.

--- backend/db/init_db.py
+++ backend/db/init_db.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 from backend.core.config import settings
 
-engine = create_engine(settings.DATABASE_URL)
+engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True, pool_size=10, max_overflow=-1, pool_recycle=3600)
 SessionLocal = sessionmaker(autocommit=False, autoflush=True, bind=engine)
 Base = declarative_base()
 
