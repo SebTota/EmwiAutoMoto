17f718b87a1cedf294638e3efc15c3461cb32e7c
Seb <kidokyo1@gmail.com>
2024-03-22 10:14:16 -0700

    Rollback db changes.

--- backend/db/init_db.py
+++ backend/db/init_db.py
@@ -4,8 +4,8 @@ from sqlalchemy.ext.declarative import declarative_base
 
 from backend.core.config import settings
 
-engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=True, bind=engine)
+engine = create_engine(settings.DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 
