2e80c6f94f40787a8c87a6af47d01367e0b4bbfb
Seb <kidokyo1@gmail.com>
2024-03-22 19:47:19 -0700

    [Bug] Encode DB password after quoting it.

--- backend/core/config.py
+++ backend/core/config.py
@@ -12,7 +12,7 @@ class Settings:
     DATABASE_USER: str = os.getenv('DATABASE_USER')
     DATABASE_PASSWORD: str = os.getenv('DATABASE_PASSWORD')
     DATABASE: str = os.getenv('DATABASE')
-    DATABASE_URL: str = f'postgresql://{DATABASE_USER}:{quote(DATABASE_PASSWORD)}@{DATABASE_HOST}/{DATABASE}'
+    DATABASE_URL: str = f'postgresql://{DATABASE_USER}:{quote(DATABASE_PASSWORD).encode()}@{DATABASE_HOST}/{DATABASE}'
 
     IMAGE_BUCKET_NAME: str = os.getenv('BUCKET_STORAGE_BUCKET_NAME')
     IMAGE_BUCKET_BASE_HOST_URL: str = os.getenv('BUCKET_STORAGE_OBJECT_BASE_URL')
